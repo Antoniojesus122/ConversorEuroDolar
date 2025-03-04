0f7fa8c48fd0673c26fcbbf4309ba339ce52a9f2
Antonio Jesús <165283213+Antoniojesus122@users.noreply.github.com>
2025-03-04 13:35:03 +0100

    Proyecto desarrollado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [valor, setValor] = useState(null);
+  const [resultado, setResultado] = useState(null);
+  
+  const converter = () => {
+    setResultado(valor * 1.17);
+  }
+
+  const [explicacion, setExplicacion] = useState(false);
+  const mostrarExplicacion = () => {
+    setExplicacion(!explicacion);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{paddingTop: "2rem"}} className='titulo'>Conversor de Euro a Dólar</h1>
+      <input className='añadirValor' type="number" onChange={e => setValor(e.target.value)} />
+      <button className='button' onClick={converter}>Convertir</button>
+      <div> {resultado!=null &&
+      <h2 className='resultado'>{resultado} dólares</h2>}
+      </div>
+      <div>
+        {resultado!=null && 
+      <button className='button' onClick={mostrarExplicacion}>Ver explicación</button>}
+      </div>
+      {explicacion!=false && <p>El valor de 1 euro en dólares es de 1.17, por lo tanto, solo debemos multiplicar el valor de euros seleccionado por 1.17</p>}
     </div>
   );
+
 }
 
 export default App;
