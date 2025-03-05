2ded32d90d160733c40f6a5cf254bffeea393ce8
Antonio Jesús <165283213+Antoniojesus122@users.noreply.github.com>
2025-03-05 11:40:16 +0100

    App conectada con Api externa
    Ahora los valores se actualizan automáticamente según cuanto sea 1 euro en dolar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const [valor, setValor] = useState(null);
   const [resultado, setResultado] = useState(null);
-  
+  const [valorcambio, setValorCambio] = useState(null);
+
+  useEffect(() => {
+    const llamadaApiCambio = async () => {
+      try {
+        const respuesta = await fetch ("https://v6.exchangerate-api.com/v6/e97356b1a09e09f0af8ef658/latest/EUR")
+        const datos = await respuesta.json();
+
+        setValorCambio(datos.conversion_rates.USD);
+
+      } catch (error){
+        console.log("Error al acceder a la API", error);
+      }
+  };
+
+    llamadaApiCambio();
+
+}, []);
+
+
+
   const converter = () => {
-    setResultado(valor * 1.17);
+    setResultado(valor * valorcambio);
   }
 
   const [explicacion, setExplicacion] = useState(false);
@@ -26,7 +46,7 @@ function App() {
         {resultado!=null && 
       <button className='button' onClick={mostrarExplicacion}>Ver explicación</button>}
       </div>
-      {explicacion!=false && <p>El valor de 1 euro en dólares es de 1.17, por lo tanto, solo debemos multiplicar el valor de euros seleccionado por 1.17</p>}
+      {explicacion!==false && <p>El valor de 1 euro en dólares es de 1.17, por lo tanto, solo debemos multiplicar el valor de euros seleccionado por 1.17</p>}
     </div>
   );
 
